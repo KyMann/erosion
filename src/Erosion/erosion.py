9886720ea8d18bad530f9b54bf36a7f1dbfade94
Kymann <akavir7@gmail.com>
2021-03-31 12:43:55 -0500

    set up testing, started erosion function

---
+++ src/Erosion/erosion.py
@@ -0,0 +1,15 @@
+
+
+def calculateSlopeVectorField(point):
+    #for a point add up the surrounding vectors
+    pass
+
+def findVector(pointA, pointB):
+    # need to break into 3 parts? x, y, z? 
+    pass
+
+def erode(eleveationMapCSV):
+    #for each point - calculate the slope with the points around it
+
+    #for each point - move materail based on slope and rainfall
+    pass
---
+++ test/Erosion/testErosion.py
@@ -0,0 +1,10 @@
+import unittest
+
+class testErosion(unittest.TestCase):
+
+    def test_test(self):
+        self.assertEqual(0,0)
+
+
+if __name__ == "__main__":
+    unittest.main()
